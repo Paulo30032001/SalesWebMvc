0beaa8113b53aa642177d41537c59ba406f6323a
Paulo30032001 <paulo.eleison@gmail.com>
2022-01-25 19:09:37 -0300

    First Controller and Razor pages Test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web mvc App from c# Course.";
+            ViewData["email"] = "paulo.eleison@gmail.com";
             return View();
         }
 
