00d8cc08ed7c0f2587dd4f408226ce27fb8c7eb1
Paulo30032001 <paulo.eleison@gmail.com>
2022-01-26 13:33:12 -0300

    MySql adptation and first migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -6,11 +6,6 @@
 
         public string Name { get; set; }
 
-        public Department(int Id, string Name)
-        {
-            this.Id = Id;
-            this.Name = Name;
-        }
-
+     
     }
 }
