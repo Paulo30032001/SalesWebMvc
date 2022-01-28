52197ddf12c36a0c4b78149d332ef4dfcdc95b01
Paulo30032001 <paulo.eleison@gmail.com>
2022-01-28 12:30:10 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
@@ -15,5 +16,20 @@ namespace SalesWebMvc.Controllers
             var list = sellerService.FindAll();
             return View(list);
         }
+        // Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller service)
+        {
+            sellerService.Insert(service);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
