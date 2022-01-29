e772a2f3077a508df61804964cc6a0b3e42bca6a
Paulo30032001 <paulo.eleison@gmail.com>
2022-01-29 13:56:48 -0300

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -36,6 +36,25 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if(id == null) { return NotFound(); }
+
+            var obj = sellerService.FindById(id.Value);
+            if(obj == null) { return NotFound(); }
+
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
 
     }
 }
